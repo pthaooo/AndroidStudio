cab932a3ddb673c47a5e8ae8f55cef21738dce82
Le Phuong Thao <lpt@gmail.com>
2024-03-01 11:59:40 +0700

    add bt4

---
+++ Baitap4/app/src/main/java/com/example/baitap4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.baitap4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.linear_layout1);
+    }
+}
